c9b546dc0f86943d18e8339765808c21723f3d6b
Matt Stavola <mstavola9@gmail.com>
2016-06-16 15:27:07 -0400

    Add process subcommand and shuffle code

--- voltaire.py
+++ voltaire.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
-import sys, os, argparse
-from sys import platform as _platform
+import argparse
+import os
+import sys
 from subprocess import call
+from sys import platform as _platform
 
-# Commands to run
-commandlist = ["pslist", "pstree", "netscan", "psxview", "consoles", "psscan", "mutantscan", "cmdscan"]
+# OS we're using
+is_windows = _platform == "win32"
 
 # Valid profiles
-validprofiles = dict.fromkeys(
+valid_profiles = dict.fromkeys(
     ["VistaSP0x64", "VistaSP0x86", "VistaSP1x64", "VistaSP1x86", "VistaSP2x64", "VistaSP2x86", "Win10x64",
      "Win10x86", "Win2003SP0x86", "Win2003SP1x64", "Win2003SP1x86", "Win2003SP2x64", "Win2003SP2x86",
      "Win2008R2SP0x64", "Win2008R2SP1x64", "Win2008SP1x64", "Win2008SP1x86", "Win2008SP2x64",
@@ -16,33 +18,7 @@ validprofiles = dict.fromkeys(
      "WinXPSP2x64", "WinXPSP2x86", "WinXPSP3x86"])
 
 
-def volcall(program, command, args):
-    print("Starting {command}".format(command=command))
-
-    path = args["dest"] + os.sep
-    outfile = "{path}ES{number}_{command}.txt".format(path=path, number=args["es"], command=command)
-
-    if "profile" in args:
-        params = "-f {src} --profile={profile} {command} --output-file={dest}".format(src=args["src"],
-                                                                                      profile=args["profile"],
-                                                                                      command=command,
-                                                                                      dest=outfile)
-    else:
-        params = "-f {src} {command} --output-file={dest}".format(src=args["src"], command=command, dest=outfile)
-
-    return call("{program} {params}".format(program=program, params=params))
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Batches common Volatility commands")
-    parser.add_argument("-s", "--src", help="Input file", required=True)
-    parser.add_argument("-d", "--dest", help="Output directory", required=True)
-
-    parser.add_argument("-p", "--profile", help="Profile name", required=False)
-    parser.add_argument("-e", "--es", help="ES mode", default=1, required=False)
-
-    args = vars(parser.parse_args())
-
+def scan(args):
     args["src"] = os.path.abspath(args["src"])
     args["dest"] = os.path.abspath(args["dest"])
 
@@ -53,7 +29,7 @@ if __name__ == "__main__":
         print("Destination directory: {dest}".format(dest=args["dest"]))
 
     if "profile" in args:
-        if args["profile"] in validprofiles:
+        if args["profile"] in valid_profiles:
             print("Profile name: {profile}".format(profile=args["profile"]))
         else:
             print("Profile not valid: {profile}".format(profile=args["profile"]))
@@ -66,18 +42,30 @@ if __name__ == "__main__":
     else:
         print("NOTICE: No ES set. Defaulting to ES=1.")
 
-    windows = _platform == "win32"
+    program = os.path.abspath("vol.exe") if is_windows else "vol.py"
 
-    program = os.path.abspath("vol.exe") if windows else "vol.py"
+    for command in ["pslist", "pstree", "netscan", "psxview", "consoles", "psscan", "mutantscan", "cmdscan"]:
+        print("Starting {command}".format(command=command))
 
-    for command in commandlist:
-        result = volcall(program, command, args)
+        path = args["dest"] + os.sep
+        outfile = "{path}ES{number}_{command}.txt".format(path=path, number=args["es"], command=command)
+
+        if "profile" in args:
+            params = "-f {src} --profile={profile} {command} --output-file={dest}".format(src=args["src"],
+                                                                                          profile=args["profile"],
+                                                                                          command=command,
+                                                                                          dest=outfile)
+        else:
+            params = "-f {src} {command} --output-file={dest}".format(src=args["src"], command=command, dest=outfile)
+
+        result = call("{program} {params}".format(program=program, params=params))
+
         if result == 0:
             print("Completed {command}".format(command=command))
         else:
             print("Error running {command}".format(command=command))
 
-    if windows:
+    if is_windows:
         path = args["dest"] + os.sep
         outfile = "{path}ES{number}_autorun.txt".format(path=path, number=args["es"])
 
@@ -97,3 +85,37 @@ if __name__ == "__main__":
 
     print("Volatility files saved to {dest}".format(dest=args["dest"]))
 
+
+def process(args):
+    args["src"] = os.path.abspath(args["src"])
+    args["dest"] = os.path.abspath(args["dest"])
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Batches common Volatility commands")
+    sub_parsers = parser.add_subparsers(dest="subparser_name")  # this line changed
+
+    scan_parser = sub_parsers.add_parser('scan')
+    scan_parser.set_defaults(which="scan")
+
+    scan_parser.add_argument("-s", "--src", help="Input file", required=True)
+    scan_parser.add_argument("-d", "--dest", help="Output directory", required=True)
+    scan_parser.add_argument("-p", "--profile", help="Profile name", required=False)
+    scan_parser.add_argument("-e", "--es", help="ES mode", default=1, required=False)
+
+    process_parser = sub_parsers.add_parser("process")
+    process_parser.set_defaults(which="process")
+
+    process_parser.add_argument("-s", "--src", help="Input directory", required=True)
+    process_parser.add_argument("-d", "--dest", help="Output directory", required=True)
+
+    args = vars(parser.parse_args())
+
+    subcommand = args.get("which", "")
+
+    if "scan" == subcommand:
+        scan(args)
+    elif "process" == subcommand:
+        process(args)
+    else:
+        parser.print_help()
